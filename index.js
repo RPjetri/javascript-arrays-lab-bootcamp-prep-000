e5815ac5ca2f1f1205af9021c4d30e92c4606667
Robert Pjetri <rpjetri@hotmail.com>
2018-08-14 14:54:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+}
